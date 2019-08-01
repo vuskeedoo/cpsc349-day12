a08835b55d7bf0774f6a582c9940ec7a3b6bcaed
JBalli12 <32500837+JBalli12@users.noreply.github.com>
2019-08-01 15:30:55 -0700

    update

--- images.js
+++ images.js
@@ -1,18 +1,17 @@
 
-let json;
-
 $.ajax({
   url: 'http://localhost:3000/images',
   dataType: 'json',
   success: function (resp) {
-    json = resp[0];
     console.log(resp);
-    let list = document.getElementById('changeMe');
-    let liTag = document.createElement('li');
-    let imageTag = document.createElement('img');
-    imageTag.src = json.src;
-    liTag.appendChild(imageTag);
-    list.appendChild(liTag);
+    for (let i = 0; i < 5; i++) {
+      let list = document.getElementById('changeMe');
+      let liTag = document.createElement('li');
+      let imageTag = document.createElement('img');
+      imageTag.src = resp[0].src;
+      liTag.appendChild(imageTag);
+      list.appendChild(liTag);
+    }
   },
   error: function (req, status, err) {
     console.log('something went wrong', status, err);
