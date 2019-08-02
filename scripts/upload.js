3d672155cfe2b88cad60a08bb94d5c2c1293baf0
NotAnna <anna.kolev@yahoo.com>
2019-08-01 20:25:10 -0700

    eslint fixes

--- scripts/upload.js
+++ scripts/upload.js
@@ -1,56 +1,49 @@
 
-let button = document.getElementById('buttonid');
+const button = document.getElementById('buttonid');
 
-function updateDB(filename, caption) {
-    $.getJSON('db/db.json', function (data) {
-
-        let jsonBody = {"id":data['images'].length+1,"src":"http://localhost:9000/images/"+filename,"caption":caption};
-        $.ajax({
-          url: 'http://localhost:3000/images',
-          type: "POST",
-          data: JSON.stringify(jsonBody),
-          contentType: "application/json; charset=utf-8",
-          dataType: "json",
-          success: function (data, textStatus, jqXHR) {
-              console.log('success');
-          }
-      });
-
-
+function updateDB (filename, caption) {
+  $.getJSON('db/db.json', function (data) {
+    const jsonBody = { id: data['images'].length + 1, src: 'http://localhost:9000/images/' + filename, caption: caption };
+    $.ajax({
+      url: 'http://localhost:3000/images',
+      type: 'POST',
+      data: JSON.stringify(jsonBody),
+      contentType: 'application/json; charset=utf-8',
+      dataType: 'json',
+      success: function (data, textStatus, jqXHR) {
+        console.log('success');
+      }
     });
+  });
 }
 
-let s3Client = new AWS.S3({
-    endpoint: 'http://127.0.0.1:9000',
-    s3ForcePathStyle: true,
-    signatureVersion: 'v4',
-    accessKeyId: 'D9C0O3KRRXB8CLPSLHNG',
-    secretAccessKey: 'Iue9V1otkpo7RLlpp8xaKS0BStBtXcAUMhW95gIq'
+const s3Client = new AWS.S3({
+  endpoint: 'http://127.0.0.1:9000',
+  s3ForcePathStyle: true,
+  signatureVersion: 'v4',
+  accessKeyId: 'D9C0O3KRRXB8CLPSLHNG',
+  secretAccessKey: 'Iue9V1otkpo7RLlpp8xaKS0BStBtXcAUMhW95gIq'
 
 });
 
 console.log(button);
 
-
-
-
-
 button.addEventListener('click', function () {
-    file = document.getElementById('file').files[0];
-    caption = document.getElementById('caption').value;
-    //console.log(this.file.name);
-    //console.log(caption);
-    s3Client.putObject({
-        Bucket: 'images',
-        Key: file.name,
-        Body: file,
-        ContentLength: file.size,
-        ContentType: file.type
-    }, function (err, data) {
-        console.log(err, data);
-    });
-    event.preventDefault();
+  file = document.getElementById('file').files[0];
+  caption = document.getElementById('caption').value;
+  // console.log(this.file.name);
+  // console.log(caption);
+  s3Client.putObject({
+    Bucket: 'images',
+    Key: file.name,
+    Body: file,
+    ContentLength: file.size,
+    ContentType: file.type
+  }, function (err, data) {
+    console.log(err, data);
+  });
+  event.preventDefault();
 
-    //console.log(document.getElementById('cation'));
-    updateDB(file.name, caption);
+  // console.log(document.getElementById('cation'));
+  updateDB(file.name, caption);
 });
